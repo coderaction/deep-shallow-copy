6cd3a3f8a0e312827a25fa7601f4c89b59f7942e
serkaneren <serkanerengit@outlook.com>
2020-09-10 18:00:48 +0300

    Ref and Value Type written explanation about

--- ShallowCopy/Program.cs
+++ ShallowCopy/Program.cs
@@ -2,11 +2,36 @@
 
 namespace ShallowCopy
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //-------------------------------------VALUE TYPE NEDİR? -------------------------------------
+            
+            //Value Type: Veriyi Ram'in stack bölgesinde tutuan veri tipleridir.
+            
+            //@@@@@@ VALUE TYPE 'LER NELERDİR ?
+            //Value Type: int, long,float,doublee, decimal, char, bool, byte, short, struct, enum
+            
+            //@@@@@@ VALUE TYPE ÖZELLİKLERİ?
+            //Null değer alamazlar
+            //New anahtarı ile oluşturulamazlar
+            
+            
+            //-------------------------------------REFERANCE TYPE NEDİR? -------------------------------------
+            
+            //Referance Type: Referans tipli bir değişken tanımlandığında, değişenin değeri belleğin heap bölgesinde tutulur.
+            //Stack bölgesinde ise değişkenin heap bölgesinde adresi tutulur. 
+            
+            //@@@@@@ REFERANCE TYPE'LER NELERDİR? 
+            //Referance Type: string, object, class, interface, array, delegate,pointer
+            
+            //@@@@@@ REFERANCE TYPE ÖZELLİKLERİ?
+            //Null değer alabilirler
+            //Ref değeri içinde object değeri olduğu new anahtar sözcüğü ile yaratılabilirler
+            
+            
+            
         }
     }
 }
